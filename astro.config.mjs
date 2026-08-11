56c461a8e14ee4b343056f2edfaeb61a72692b2c
Antonio Herrera <herreraquinteroj@gmail.com>
2026-08-11 13:41:30 -0700

    seo2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://esparragosdelosplanes.com/',
   vite: {
     plugins: [tailwindcss()],
   },
+    integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: 'es',
+        locales: {
+          es: 'es-MX',
+          en: 'en-US',
+        },
+      },
+    }),
+  ],
 });
